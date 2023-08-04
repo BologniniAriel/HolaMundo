1e25301cb777ad73a2d61468c80d4fb1825cc1ff
NAZARETH ARIEL BOLOGNINI <nbolognini@unimoron.edu.ar>
2023-08-04 19:58:07 -0300

    Agregue una clase nueva pero no la agregue al Stage

---
+++ HolaMundoOtro/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundoOtro
+{
+    internal class Class1
+    {
+        //Console.Writeline("HOLA OTRA VES EN OTRA CLASE");
+    }
+}
